e37795d07f9908e6d8d5e466b5f55131dd66bcc0
Kingooo <kingodogo@outlook.com>
2025-12-30 12:09:04 +0530

    Initial empty commit

--- src/main/java/com/kingodogo/buildscape/block/FreshCopperBulbBlock.java
+++
@@ -1,8 +0,0 @@
-package com.kingodogo.buildscape.block;
-
-public class FreshCopperBulbBlock extends CopperBulbBlock {
-
-    public FreshCopperBulbBlock(Properties properties) {
-        super(properties, 15);
-    }
-}
--- src/main/java/com/kingodogo/buildscape/block/WeatheredCopperBulbBlock.java
+++
@@ -1,8 +0,0 @@
-package com.kingodogo.buildscape.block;
-
-public class WeatheredCopperBulbBlock extends CopperBulbBlock {
-
-    public WeatheredCopperBulbBlock(Properties properties) {
-        super(properties, 8);
-    }
-}

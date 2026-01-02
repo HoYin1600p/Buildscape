42117369a318c71c22267215ca33ed3e23b814a2
Kingooo <kingodogo@outlook.com>
2026-01-02 09:26:28 +0530

    cleanup

--- src/main/java/com/kingodogo/buildscape/world/ModGameRules.java
+++ src/main/java/com/kingodogo/buildscape/world/ModGameRules.java
@@ -8,15 +8,12 @@ public class ModGameRules {
 
     public static void register() {
         try {
-            // Access the private create method using reflection
             Method createMethod = GameRules.BooleanValue.class.getDeclaredMethod("create", boolean.class);
             createMethod.setAccessible(true);
-            
-            // Create the boolean value type with default value false
-            GameRules.Type<GameRules.BooleanValue> booleanType = 
+
+            GameRules.Type<GameRules.BooleanValue> booleanType =
                     (GameRules.Type<GameRules.BooleanValue>) createMethod.invoke(null, false);
-            
-            // Register the gamerule
+
             FAST_LEAF_DECAY = GameRules.register(
                     "fastLeafDelay",
                     GameRules.Category.UPDATES,
